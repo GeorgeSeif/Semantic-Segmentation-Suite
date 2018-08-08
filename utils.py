2dcc18ef24bff002f85a7dc8309f9d6a5b6739fb
Dave Dolson <dave@darwinai.ca>
2018-08-08 16:28:49 -0400

    Improve crop error message

--- utils.py
+++ utils.py
@@ -147,7 +147,7 @@ def random_crop(image, label, crop_height, crop_width):
         else:
             return image[y:y+crop_height, x:x+crop_width, :], label[y:y+crop_height, x:x+crop_width]
     else:
-        raise Exception('Crop shape exceeds image dimensions!')
+        raise Exception('Crop shape (%d, %d) exceeds image dimensions (%d, %d)!' % (crop_height, crop_width, image.shape[0], image.shape[1]))
 
 # Compute the average segmentation accuracy across all classes
 def compute_global_accuracy(pred, label):
