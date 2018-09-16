0b375534d1587f7623f47a69e192ef89d4239ee2
Joel Teply <joel@cambriantech.com>
2018-09-16 03:00:55 -0500

    just resize

--- utils.py
+++ utils.py
@@ -323,20 +323,25 @@ def lovasz_softmax(probas, labels, only_present=True, per_image=False, ignore=No
 def random_crop(image, label, crop_height, crop_width):
     if (image.shape[0] != label.shape[0]) or (image.shape[1] != label.shape[1]):
         raise Exception('Image and label must have the same dimensions!')
-        
-    if (crop_width <= image.shape[1]) and (crop_height <= image.shape[0]):
-        x = random.randint(0, image.shape[1]-crop_width)
-        y = random.randint(0, image.shape[0]-crop_height)
-        
-        if len(label.shape) == 3:
-            return image[y:y+crop_height, x:x+crop_width, :], label[y:y+crop_height, x:x+crop_width, :]
-        else:
-            return image[y:y+crop_height, x:x+crop_width, :], label[y:y+crop_height, x:x+crop_width]
+
+    if len(label.shape) == 3:
+        return misc.imresize(image, (crop_height, crop_width, 3)), misc.imresize(label, (crop_height, crop_width, 3))
     else:
-        if len(label.shape) == 3:
-            return misc.imresize(image, (crop_height, crop_width, 3)), misc.imresize(label, (crop_height, crop_width, 3))
-        else:
-            return misc.imresize(image, (crop_height, crop_width, 3)), misc.imresize(label, (crop_height, crop_width))
+        return misc.imresize(image, (crop_height, crop_width, 3)), misc.imresize(label, (crop_height, crop_width))
+        
+    # if (crop_width <= image.shape[1]) and (crop_height <= image.shape[0]):
+    #     x = random.randint(0, image.shape[1]-crop_width)
+    #     y = random.randint(0, image.shape[0]-crop_height)
+        
+    #     if len(label.shape) == 3:
+    #         return image[y:y+crop_height, x:x+crop_width, :], label[y:y+crop_height, x:x+crop_width, :]
+    #     else:
+    #         return image[y:y+crop_height, x:x+crop_width, :], label[y:y+crop_height, x:x+crop_width]
+    # else:
+    #     if len(label.shape) == 3:
+    #         return misc.imresize(image, (crop_height, crop_width, 3)), misc.imresize(label, (crop_height, crop_width, 3))
+    #     else:
+    #         return misc.imresize(image, (crop_height, crop_width, 3)), misc.imresize(label, (crop_height, crop_width))
         # raise Exception('Crop shape (%d, %d) exceeds image dimensions (%d, %d)!' % (crop_height, crop_width, image.shape[0], image.shape[1]))
 
 # Compute the average segmentation accuracy across all classes
