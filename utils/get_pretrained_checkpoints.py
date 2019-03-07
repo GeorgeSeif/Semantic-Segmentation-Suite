f96f4d95d117225fd375df6b8508a44e73a310de
GeorgeSeif <george.seif94@gmail.com>
2019-03-06 22:15:39 -0500

    Changed model download links

--- utils/get_pretrained_checkpoints.py
+++ utils/get_pretrained_checkpoints.py
@@ -1,7 +1,5 @@
 import subprocess
 import argparse
-import os
-import shutil
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--model', type=str, default="ALL", help='Which model weights to download')
@@ -9,28 +7,47 @@ args = parser.parse_args()
 
 
 if args.model == "ResNet50" or args.model == "ALL":
-	subprocess.check_output(['wget','https://s3.amazonaws.com/pretrained-weights/resnet_v2_50.ckpt', "-P", "models"])
+	subprocess.check_output(['wget','http://download.tensorflow.org/models/resnet_v2_50_2017_04_14.tar.gz', "-P", "models"])
+	try:
+		subprocess.check_output(['tar', '-xvf', 'models/resnet_v2_50_2017_04_14.tar.gz', "-C", "models"])
+		subprocess.check_output(['rm', 'models/resnet_v2_50_2017_04_14.tar.gz'])
+	except Exception as e:
+		print(e)
+		pass
 
 if args.model == "ResNet101" or args.model == "ALL":
-	subprocess.check_output(['wget','https://s3.amazonaws.com/pretrained-weights/resnet_v2_101.ckpt', "-P", "models"])
+	subprocess.check_output(['wget','http://download.tensorflow.org/models/resnet_v2_101_2017_04_14.tar.gz', "-P", "models"])
+	try:
+		subprocess.check_output(['tar', '-xvf', 'models/resnet_v2_101_2017_04_14.tar.gz', "-C", "models"])
+		subprocess.check_output(['rm', 'models/resnet_v2_101_2017_04_14.tar.gz'])
+	except Exception as e:
+		print(e)
+		pass
 
 if args.model == "ResNet152" or args.model == "ALL":
-	subprocess.check_output(['wget','https://s3.amazonaws.com/pretrained-weights/resnet_v2_152.ckpt', "-P", "models"])
+	subprocess.check_output(['wget','http://download.tensorflow.org/models/resnet_v2_152_2017_04_14.tar.gz', "-P", "models"])
+	try:
+		subprocess.check_output(['tar', '-xvf', 'models/resnet_v2_152_2017_04_14.tar.gz', "-C", "models"])
+		subprocess.check_output(['rm', 'models/resnet_v2_152_2017_04_14.tar.gz'])
+	except Exception as e:
+		print(e)
+		pass
 
 if args.model == "MobileNetV2" or args.model == "ALL":
-	subprocess.check_output(['wget','https://s3.amazonaws.com/pretrained-weights/mobilenet_v2.ckpt.meta', "-P", "models"])
-	subprocess.check_output(['wget','https://s3.amazonaws.com/pretrained-weights/mobilenet_v2.ckpt.index', "-P", "models"])
-	subprocess.check_output(['wget','https://s3.amazonaws.com/pretrained-weights/mobilenet_v2.ckpt.data-00000-of-00001', "-P", "models"])
+	subprocess.check_output(['wget','https://storage.googleapis.com/mobilenet_v2/checkpoints/mobilenet_v2_1.4_224.tgz', "-P", "models"])
+	try:
+		subprocess.check_output(['tar', '-xvf', 'models/mobilenet_v2_1.4_224.tgz', "-C", "models"])
+		subprocess.check_output(['rm', 'models/mobilenet_v2_1.4_224.tgz'])
+	except Exception as e:
+		print(e)
+		pass
 
 if args.model == "InceptionV4" or args.model == "ALL":
-	subprocess.check_output(['wget','https://s3.amazonaws.com/pretrained-weights/inception_v4.ckpt', "-P", "models"])
-	
-if args.model == "SEResNeXt50" or args.model == "ALL":
-	subprocess.check_output(['wget','https://s3.amazonaws.com/pretrained-weights/se_resnext50.ckpt.meta', "-P", "models"])
-	subprocess.check_output(['wget','https://s3.amazonaws.com/pretrained-weights/se_resnext50.ckpt.index', "-P", "models"])
-	subprocess.check_output(['wget','https://s3.amazonaws.com/pretrained-weights/se_resnext50.ckpt.data-00000-of-00001', "-P", "models"])
-
-if args.model == "SEResNeXt101" or args.model == "ALL":
-	subprocess.check_output(['wget','https://s3.amazonaws.com/pretrained-weights/se_resnext101.ckpt.meta', "-P", "models"])
-	subprocess.check_output(['wget','https://s3.amazonaws.com/pretrained-weights/se_resnext101.ckpt.index', "-P", "models"])
-	subprocess.check_output(['wget','https://s3.amazonaws.com/pretrained-weights/se_resnext101.ckpt.data-00000-of-00001', "-P", "models"])
+	subprocess.check_output(
+		['wget', 'http://download.tensorflow.org/models/inception_v4_2016_09_09.tar.gz', "-P", "models"])
+	try:
+		subprocess.check_output(['tar', '-xvf', 'models/inception_v4_2016_09_09.tar.gz', "-C", "models"])
+		subprocess.check_output(['rm', 'models/inception_v4_2016_09_09.tar.gz'])
+	except Exception as e:
+		print(e)
+		pass
